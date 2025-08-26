443155d89e129e8651fbcc5121b0679d3cfb97ef
Alejandro Cruz Cerna Araiza <nohizelatarea@gmail.com>
2025-08-26 06:09:56 -0600

    added HEAPU8 runtime method

--- compile.sh
+++ compile.sh
@@ -15,7 +15,7 @@ emcc -O3 \
     -s SAFE_HEAP=1 \
     -s STACK_OVERFLOW_CHECK=2 \
     -s EXPORTED_FUNCTIONS="['_initiate_download','_get_decompressed_data','_get_decompressed_size','_get_error_message','_get_error_code','_free_resources','_init_module']" \
-    -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap','UTF8ToString']" \
+    -s EXPORTED_RUNTIME_METHODS="['ccall','cwrap','UTF8ToString', 'HEAPU8']" \
     -s FILESYSTEM=0 \
     -s MODULARIZE=1 \
     -s EXPORT_ES6=1 \
